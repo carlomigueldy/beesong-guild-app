3f77063e4bda1e1362167f725d5b8b5ab6d480cf
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-05-25 19:37:59 +0800

    adds constant colors

--- lib/styles/themes.dart
+++ lib/styles/themes.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+final kcDarkGray = Colors.grey[900];
+final kcDarkLightGray = Colors.grey[800];
+final kcSuccess = Colors.green;
+
 List<ThemeData> themes = [
   ThemeData(
-    primaryColor: Colors.blue,
-    primarySwatch: Colors.blue,
+    primaryColor: Colors.red[900],
     fontFamily: GoogleFonts.montserrat().fontFamily,
+    brightness: Brightness.dark,
+    appBarTheme: AppBarTheme(backgroundColor: kcDarkGray),
   ),
   ThemeData(primaryColor: Colors.red),
   ThemeData(primaryColor: Colors.green),
