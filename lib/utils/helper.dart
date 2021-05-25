d140b30e3d7302afce093480d88cd42f89c4c848
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-05-25 19:37:09 +0800

    added asset helper

---
+++ lib/utils/helper.dart
@@ -0,0 +1,12 @@
+abstract class Assets {
+  static const DEATH_KNIGHT = 'assets/classes/death_knight.png';
+  static const DRUID = 'assets/classes/druid.png';
+  static const HUNTER = 'assets/classes/hunter.png';
+  static const MAGE = 'assets/classes/mage.png';
+  static const PALADIN = 'assets/classes/paladin.png';
+  static const PRIEST = 'assets/classes/priest.png';
+  static const ROGUE = 'assets/classes/rogue.png';
+  static const SHAMAN = 'assets/classes/shaman.png';
+  static const WARLOCK = 'assets/classes/warlock.png';
+  static const WARRIOR = 'assets/classes/warrior.png';
+}
